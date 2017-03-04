04c814ab749f052750e4ae647927eb9934d562c1
YoannBa <yoann.baunach@hetic.net>
2017-03-04 09:18:51 +0100

    :sparkles: index > Add HTML structure

--- index.php
+++ index.php
@@ -16,7 +16,50 @@
 
     <body>
 
+        <header>
+            <div class="logo">
+                <div class="logo-image"></div>
+                <h1 class="logo-text">Inventory</h1>
+            </div>
+        </header>
 
+        <div class="landing-presentation">
+            <h2 class="hero-title">Keep track of your products</h2>
+            <p class="hero-subtitle">The most efficient tool to manage your inventory online</p>
+            <div class="landing-img"></div>
+            <div class="landing-access">
+                <a href="#" class="button connection"></a>
+                <a href="#" class="button sign-up"></a>
+            </div>
+        </div>
+
+        <div class="connexion pop-in">
+            <div class="pop-tile">
+                <form action="#" method="post">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" class="input-text">
+                    <label for="password">Password</label>
+                    <input type="text" id="password" class="input-text">
+                    <input type="submit" id="username" class="input-submit">
+                </form>
+            </div>
+        </div>
+
+        <div class="sign-up pop-in">
+            <div class="pop-tile">
+                <form action="#" method="post">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" class="input-text">
+                    <label for="mail">E-mail</label>
+                    <input type="text" id="mail" class="input-text">
+                    <label for="password">Password</label>
+                    <input type="text" id="password" class="input-text">
+                    <label for="confirm_pass">Confirm Password</label>
+                    <input type="text" id="confirm_pass" class="input-text">
+                    <input type="submit" id="username" class="input-submit">
+                </form>
+            </div>
+        </div>
 
         <script src="assets/js/main.js"></script>
     </body>
