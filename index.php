75cf6bb47d4ad67a6cd4e7c0c10c07bb1bb32e49
YoannBa <yoann.baunach@hetic.net>
2017-03-04 08:50:08 +0100

    :tada: Init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Inventory</title>
+        <meta name="description" content="The best tool to manage your inventory online" />
+
+        <!-- Links -->
+        <link href="assets/css/reset.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+
+
+
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
