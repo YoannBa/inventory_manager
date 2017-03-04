1742e2d7469aec8dbb73cf1e62278139d5b676ea
YoannBa <yoann.baunach@hetic.net>
2017-03-04 10:47:26 +0100

    :bug: index > Add missing code for css

--- index.php
+++ index.php
@@ -10,17 +10,18 @@
 
         <!-- Links -->
         <link href="assets/css/reset.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/general.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/index.css" rel="stylesheet" type="text/css" />
 
     </head>
 
     <body>
 
         <header>
-            <div class="logo">
+            <a href="#" class="logo">
                 <div class="logo-image"></div>
                 <h1 class="logo-text">Inventory</h1>
-            </div>
+            </a>
         </header>
 
         <div class="landing-presentation">
@@ -28,35 +29,37 @@
             <p class="hero-subtitle">The most efficient tool to manage your inventory online</p>
             <div class="landing-img"></div>
             <div class="landing-access">
-                <a href="#" class="button connection"></a>
-                <a href="#" class="button sign-up"></a>
+                <a href="#" class="button-1 button connection">Connect</a>
+                <a href="#" class="button-2 button sign-up">Sign up</a>
             </div>
         </div>
 
         <div class="connexion pop-in">
             <div class="pop-tile">
+                <div class="pop-in-close"></div>
                 <form action="#" method="post">
                     <label for="username">Username</label>
-                    <input type="text" id="username" class="input-text">
+                    <input type="text" id="username" class="input-text" placeholder="John Doe">
                     <label for="password">Password</label>
-                    <input type="text" id="password" class="input-text">
-                    <input type="submit" id="username" class="input-submit">
+                    <input type="password" id="password" class="input-text" placeholder="********">
+                    <input type="submit" id="username" class="input-submit button-1">
                 </form>
             </div>
         </div>
 
         <div class="sign-up pop-in">
             <div class="pop-tile">
+                <div class="pop-in-close"></div>
                 <form action="#" method="post">
                     <label for="username">Username</label>
-                    <input type="text" id="username" class="input-text">
+                    <input type="text" id="username" class="input-text" placeholder="John Doe">
                     <label for="mail">E-mail</label>
-                    <input type="text" id="mail" class="input-text">
+                    <input type="email" id="mail" class="input-text" placeholder="john.doe@inventory.com">
                     <label for="password">Password</label>
-                    <input type="text" id="password" class="input-text">
+                    <input type="password" id="password" class="input-text" placeholder="********">
                     <label for="confirm_pass">Confirm Password</label>
-                    <input type="text" id="confirm_pass" class="input-text">
-                    <input type="submit" id="username" class="input-submit">
+                    <input type="password" id="confirm_pass" class="input-text" placeholder="********">
+                    <input type="submit" id="username" class="input-submit button button-1" >
                 </form>
             </div>
         </div>
