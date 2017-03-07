aaf3c220b2354eb25e6481fcc0c2aa3d8b3fe54b
YoannBa <yoann.baunach@hetic.net>
2017-03-07 10:50:22 +0100

    :sparkles: php > Login and user handling

---
+++ assets/php/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Connexion variables
+define('DB_HOST','localhost');
+define('DB_NAME','inventory');
+define('DB_USER','root');
+define('DB_PASS','root'); // '' par défaut sur windows
+
+try
+{
+    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+}
+catch (Exception $e)
+{
+    die('Could not connect');
+}
+                    
