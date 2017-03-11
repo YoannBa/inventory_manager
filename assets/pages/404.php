e081c48b5fba88c684de613e1171eab59e8411f4
YoannBa <yoann.baunach@hetic.net>
2017-03-11 09:40:01 +0100

    :lipstick: css/pages > 404 design

--- assets/pages/404.php
+++ assets/pages/404.php
@@ -1,1 +1,24 @@
-<h1>Perdu ?</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Inventory - Dashboard</title>
+        <meta name="description" content="Your dashboard - Manage all your inventory from here" />
+
+        <!-- Links -->
+        <link href="assets/css/reset.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/general.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/404.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+    	<div class="container-404">
+    		<h1>ヅ Are you lost ? ヅ</h1>
+			<a href="home" class="button button-1" >Go back to home page</a>
+    	</div>
+    </body>
+</html>
+    
